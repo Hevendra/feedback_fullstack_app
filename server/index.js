f2085650b6e0386ea7156c0dd144691e1bfbb26b
VJADAHE <hevendrasingh.jadaun@pearson.com>
2020-05-10 22:52:30 +0530

    implemented google auth login

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+require('./services/passport') // Just we need to load this file
+
 
 const app = express();
-
-app.get('/',(req, res) => {
-    res.send({hi : 'there'});
-});
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
