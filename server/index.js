9082596b67ac125ac74e9759a7cb8f555e0b6654
VJADAHE <hevendrasingh.jadaun@pearson.com>
2020-05-11 19:22:38 +0530

    implemented mongoDB and prod configs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
-require('./services/passport') // Just we need to load this file
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+// Just we need to load these file
+require('./models/User')
+require('./services/passport') 
 
-
+mongoose.connect(keys.mongoURI)
 const app = express();
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000, //30 days
+    keys: [keys.cookieKey]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
